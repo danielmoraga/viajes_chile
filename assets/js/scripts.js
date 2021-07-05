33625742076f204705441b4b337a444907742d87
daniel <danielmoragarodriguez@gmail.com>
2021-07-05 15:54:33 -0400

    cuarto commit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,17 @@
 $(function () {
 
-  $('[data-toggle="tooltip"]').tooltip();
+  $('[data-toggle="tooltip"]').tooltip(); //uso de tooltip
 
-  $("a").on('click', function(event) {
+  $(function () {
+    $('[data-toggle="popover"]').popover() //uso de popovers
+  })
+
+  $('#myModal').on('shown.bs.modal', function () { //uso de modal
+    $('#myInput').trigger('focus')
+  })
+
+
+  $("a").on('click', function(event) { //funciones para slow scroll
 
     if (this.hash !== "") {
       event.preventDefault();
@@ -16,3 +25,21 @@ $(function () {
     });
 
 });
+
+
+(function () {
+  var qs, js, q, s, d = document,
+    gi = d.getElementById,
+    ce = d.createElement,
+    gt = d.getElementsByTagName,
+    id = "typef_orm",
+    b = "https://embed.typeform.com/";
+  if (!gi.call(d, id)) {
+    js = ce.call(d, "script");
+    js.id = id;
+    js.src = b + "embed.js";
+    q = gt.call(d, "script")[0];
+    q.parentNode.insertBefore(js, q)
+  }
+})()
+
